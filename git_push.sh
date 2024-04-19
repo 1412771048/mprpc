e44de45474fcc95b89594e0050e140d49043f376
geyulong <1412771048@qq.com>
2024-04-19 23:18:17 +0800

    push

--- git_push.sh
+++ git_push.sh
@@ -7,5 +7,11 @@ if [ ! -f "thirdparty.tgz" ]; then
 fi
 
 rm -rf thirdparty/
-
-git add . && git commit -m "push" && git pull && git push
+git add .
+git commit -m "push" 
+# 检查git commit命令的返回状态
+if [ $? -ne 0 ]; then
+    echo "no update, don't need push"
+else 
+    git pull && git push
+fi

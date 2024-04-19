9826421f9ee2156ae74d32a0c0ce824a52842ab8
geyulong <1412771048@qq.com>
2024-04-19 22:43:09 +0800

    boost

---
+++ build/src/CMakeFiles/mprpc.dir/__/thirdparty/simpleini/ConvertUTF.cc.o.d
@@ -0,0 +1,4 @@
+src/CMakeFiles/mprpc.dir/__/thirdparty/simpleini/ConvertUTF.cc.o: \
+ /home/gyl/workspace/mprpc/thirdparty/simpleini/ConvertUTF.cc \
+ /usr/include/stdc-predef.h \
+ /home/gyl/workspace/mprpc/thirdparty/simpleini/ConvertUTF.h
---
+++ build/src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o.d
@@ -0,0 +1,2 @@
+src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o: \
+ /home/gyl/workspace/mprpc/src/mprpcchannel.cc /usr/include/stdc-predef.h

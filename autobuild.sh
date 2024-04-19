4e166e92123b6b9461e2cf1bf407f5787e4cfac7
gyl <1412771048@qq.com>
2024-04-19 09:29:49 +0800

    删减库

--- autobuild.sh
+++ autobuild.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -x
 
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
 cd build/ && rm -rf * && cmake .. && make #[-j4]
---
+++ build/src/CMakeFiles/mprpc.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../../output/lib/libmprpc.a"
+  "../../output/lib/libmprpc.pdb"
+  "CMakeFiles/mprpc.dir/__/thirdparty/simpleini/ConvertUTF.cc.o"
+  "CMakeFiles/mprpc.dir/__/thirdparty/simpleini/ConvertUTF.cc.o.d"
+  "CMakeFiles/mprpc.dir/databank.cc.o"
+  "CMakeFiles/mprpc.dir/databank.cc.o.d"
+  "CMakeFiles/mprpc.dir/mprpcchannel.cc.o"
+  "CMakeFiles/mprpc.dir/mprpcchannel.cc.o.d"
+  "CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+  "CMakeFiles/mprpc.dir/rpcprovider.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mprpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

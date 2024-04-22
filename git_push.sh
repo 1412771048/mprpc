2cc68ea6426481b81245830fa665b3224c4dafd3
geyulong <1412771048@qq.com>
2024-04-22 16:08:06 +0800

    update

--- git_push.sh
+++ git_push.sh
@@ -3,7 +3,10 @@
 
 # 检查git status是否返回4行
 if [ $(git status | wc -l) != 4 ]; then
-    git add . && git commit -m "update" && git pull && git push
+    git add . 
+    git commit -m "update" || true
+    git pull 
+    git push
 else
     echo "no update, don't need push"
 fi

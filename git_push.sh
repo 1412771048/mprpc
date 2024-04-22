9bf195345a1f4268cc4419665dfddc7736d12f8c
geyulong <1412771048@qq.com>
2024-04-22 16:10:12 +0800

    update

--- git_push.sh
+++ git_push.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-
 # 检查git status是否返回4行
 if [ $(git status | wc -l) != 4 ]; then
     git add . 
-    git commit -m "update" || true
+    git commit -m "update" || true  #不论commit是否成功都向下走
     git pull 
     git push
 else

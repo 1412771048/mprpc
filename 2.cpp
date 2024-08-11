02dd7c4c52f1c8d50f43aa69df4f3babbc184436
geyulong <1412771048@qq.com>
2024-08-11 18:42:47 +0800

    修改readme

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+namespace space{
+    void func(const int a) {
+        std::cout << "space" << std::endl;
+    }
+}
+
+void func(const int a) {
+    std::cout << "func" << std::endl;
+}
+
+int main() {
+    
+}

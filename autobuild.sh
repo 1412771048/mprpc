7fa3532f2465ba105ce8c4af111b73e0ae095d97
geyulong <1412771048@qq.com>
2025-01-20 22:38:04 +0800

    modify cmake

--- autobuild.sh
+++ autobuild.sh
@@ -5,5 +5,4 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
-# cd build/ && rm -rf * && cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j16
-cd build/ && rm -rf * && cmake .. && make -j16
+cd build/ && rm -rf * && cmake .. -DCMAKE_BUILD_TYPE=release && make -j$(nproc)
